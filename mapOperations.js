0eeaf28683f5274df439911a150aaad2474c207a
Muhammad Anas <41787190+Anaskhaan@users.noreply.github.com>
2024-01-24 14:23:55 +0500

    Add files via upload

---
+++ mapOperations.js
@@ -0,0 +1,6 @@
+//    Create an array named originalArray containing at least three numbers.
+const originalArray = [4, 6, 8];
+console.log(originalArray);
+//   Use the map function to create a new array (doubledArray) containing each number doubled.
+const doubledArray = originalArray.map((Number) => Number * 2);
+console.log(doubledArray);
